51a0cee87371aeae940d44b0086da52bd4ba5391
Ogbu Ezekiel <zicozydasilva70@gmail.com>
2025-01-06 14:43:00 +0100

    chore: other conditional query

--- main.js
+++ main.js
@@ -6,3 +6,17 @@ new GlideQuery('incident')
 .forEach(function(task) {
 	gs.info(task.short_description)
 })
+
+
+new GlideQuery('cmdb_ci')
+// .where("name", "STARTSWITH", "A")
+// .where('name', 'Access')
+// .whereNotNull('name')
+// .orWhereNull('') 
+// .whereNull('manufacturer')
+.where('manufacturer')
+.select('name', "manufacturer")
+.forEach(function (ci) {
+    gs.info(JSON.stringify(ci));
+});
+
